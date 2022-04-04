7e1ac16452c48bc921540c4f6e677b90fe0cebd6
BigBadMuttuH <bigbadantonio@gmail.com>
2022-04-05 01:15:09 +0300

    hw7 исправления и доработки

--- hw7/hw7.cs
+++ hw7/hw7.cs
@@ -1,19 +1,4 @@
-﻿void PrintTowDimFloatArray(float[,] myArray)
-{
-    Console.ForegroundColor = ConsoleColor.White;
-    Console.ForegroundColor = ConsoleColor.DarkGreen;
-    for (int i = 0; i < myArray.GetLength(0); i++)
-    {
-        for (int j = 0; j < myArray.GetLength(1); j++)
-        {
-            Console.Write($"{myArray[i, j]:f2}\t");
-        }
-        Console.WriteLine();
-    }
-    Console.ForegroundColor = ConsoleColor.White;
-}
-
-void PrintTowDimintArray(int[,] myArray)
+﻿void PrintTowDimintArray(int[,] myArray)
 {
     Console.ForegroundColor = ConsoleColor.White;
     Console.ForegroundColor = ConsoleColor.DarkGreen;
@@ -29,22 +14,6 @@ void PrintTowDimintArray(int[,] myArray)
     Console.ForegroundColor = ConsoleColor.White;
 }
 
-float[,] CreateTowDimentionFloatArray(int hight, int length, int min, int max)
-{
-    float[,] towDimFloatArray = new float[hight, length];
-
-    for (int i = 0; i < hight; i++)
-    {
-        for (int j = 0; j < length; j++)
-        {
-            towDimFloatArray[i, j] = new Random().Next(min, max);
-            towDimFloatArray[i, j] *= new Random().NextSingle();
-
-        }
-    }
-
-    return towDimFloatArray;
-}
 
 int[,] CreateTowDimArray(int hight, int length, int min, int max)
 {
@@ -64,6 +33,37 @@ int[,] CreateTowDimArray(int hight, int length, int min, int max)
 // Задача 47:
 // Задайте двумерный массив размером m×n,
 // заполненный случайными вещественными числами.
+float[,] CreateTowDimentionFloatArray(int hight, int length, int min, int max)
+{
+    float[,] towDimFloatArray = new float[hight, length];
+
+    for (int i = 0; i < hight; i++)
+    {
+        for (int j = 0; j < length; j++)
+        {
+            towDimFloatArray[i, j] = new Random().Next(min, max);
+            towDimFloatArray[i, j] *= new Random().NextSingle();
+
+        }
+    }
+
+    return towDimFloatArray;
+}
+
+void PrintTowDimFloatArray(float[,] myArray)
+{
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    for (int i = 0; i < myArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < myArray.GetLength(1); j++)
+        {
+            Console.Write($"{myArray[i, j]:f2}\t");
+        }
+        Console.WriteLine();
+    }
+    Console.ForegroundColor = ConsoleColor.White;
+}
 float[,] myFloatArray = CreateTowDimentionFloatArray(3, 4, -10, 10);
 PrintTowDimFloatArray(myFloatArray);
 Console.WriteLine();
@@ -72,66 +72,86 @@ Console.WriteLine();
 // Задача 50:
 // Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
 // и возвращает значение этого элемента или же указание, что такого элемента нет.
-String isNumberPositionInArrat(int[,] someIntArray, int number)
+int GetNumberFromPositionInArray(int[,] someIntArray, int number)
 {
     int position = number;
 
     if ( number > 0 && number <= someIntArray.Length)
     {
-        int row = someIntArray.GetLength(1);
-        int col = someIntArray.GetLength(0);
+        int rowLength = someIntArray.GetLength(1);
         
         // найдем индексы нужного элемента в массиве
         int i = 0, j = 0;
-        // строка (row) ряд в котором находиься нужный элемент
-        while (number > row)
+        // строка (row ряд) в котором находиься нужный элемент
+        while (number > rowLength)
         {
-            number -= row;
+            number -= rowLength;
             i++;
         }
-        // индеск элемента в конкретном ряду
-        j = row - ( row - number ) - 1;
+        // индеск элемента в конкретном ряду (колонка)
+        j = rowLength - ( rowLength - number ) - 1;
 
-        return $"На {position} позиции в массиве находится число = {someIntArray[i, j]}";
+        return someIntArray[i, j];
     }
-    return $"позиция {position} за пределами массива";
+    // нужно вернуть число!
+    // если ни чего не получилось,
+    // то возвращем число,
+    // ктоторого точно не може быть в массиве.
+    // например "-1" для массива положительных чисел.
+
+    return -1;
 }
 
 
-int[,] myIntArray = CreateTowDimArray(hight:5, length: 4, min: 100, max:999);
+int[,] myIntArray = CreateTowDimArray(hight:3, length: 5, min: 0, max:10);
 PrintTowDimintArray(myIntArray);
 
 int position = new Random().Next(0, myIntArray.Length);
-String str50 = isNumberPositionInArrat(myIntArray, position);
+
+int number = GetNumberFromPositionInArray(myIntArray, position);
+
+String str50 = number >= 0
+    ?
+    $"На {position} позиции в массиве находится число = {number}"
+    :
+    $"позиция {position} за пределами массива";
+
 Console.WriteLine(str50);
-
-// для тестирования вывода из задачи 50
-//foreach (int item in Enumerable.Range(0, 13))
-//{
-//    Console.WriteLine(isNumberPositionInArrat(myIntArray, item));
-//}
-
 
 
 
 // Задача 52:
 // Задайте двумерный массив из целых чисел.
-// Найдите среднее арифметическое элементов в каждом столбце.``
-String GetAverage(int[,] intArray)
+// Найдите среднее арифметическое элементов в каждом столбце.
+float[] GetAverage(int[,] intArray)
 {
-    String str = String.Empty;
-    int sum = 0;
-    for (int j = 0; j < intArray.GetLength(1); j++)
+    float[] avarageArray = new float[intArray.GetLength(1)];
+    float sum = 0;
+
+    int rows = intArray.GetLength(0);
+    int columns = intArray.GetLength(1);
+    
+    // Console.WriteLine("rows {0}, columns={1}", rows, columns);
+    for (int j = 0; j < columns; j++)
     {
-        for (int i = 0; i < intArray.GetLength(0); i++)
+        for (int i = 0; i < rows; i++)
         {
             sum += intArray[i, j];
         }
-        str += $"{sum}; \t";
+        avarageArray[j] = sum / rows;
         sum = 0;
     }
-    return str;
+    return avarageArray;
 }
-String str52 = GetAverage(myIntArray);
-Console.WriteLine("Среднее арифметическое каждого столбца: ");
-Console.WriteLine(str52);
+float[] avarageArray = GetAverage(myIntArray);
+
+Console.WriteLine("Среднее арифметическое каждого столбца:");
+
+Console.ForegroundColor = ConsoleColor.Red;
+
+foreach (var item in avarageArray)
+{
+    Console.Write($"{item:f1}\t");
+}    
+
+Console.ForegroundColor = ConsoleColor.White;
